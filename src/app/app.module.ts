4f321a378a3c418194c12b3a58577f85829abd80
Raheel Jameel <raheel_jameel@yahoo.com>
2018-02-04 22:07:58 +0500

    feat(app): inital app structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,21 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { UniversityComponent } from './university/university.component';
+import { StudentComponent } from './student/student.component';
+import { VerifyComponent } from './verify/verify.component';
+import { HeaderFooterComponent } from './common/header-footer/header-footer.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    UniversityComponent,
+    StudentComponent,
+    VerifyComponent,
+    HeaderFooterComponent
   ],
   imports: [
     BrowserModule
