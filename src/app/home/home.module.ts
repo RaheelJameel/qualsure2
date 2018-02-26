3ad9862b8d4e65ee978c1c8e8c81816321cc81d0
Raheel Jameel <raheel_jameel@yahoo.com>
2018-02-27 02:04:32 +0500

    feat(home): add missing MDBBootstrapModule in home module

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,12 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
+
 import { HomeComponent } from './home.component';
 
 @NgModule({
   imports: [
     CommonModule,
     RouterModule,
+    MDBBootstrapModule.forRoot(),
   ],
   declarations: [
     HomeComponent,
