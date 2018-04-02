545f6891730113ebd0172650896ee69a6933a924
SaadatRizvi <saadatrezvi@gmail.com>
2018-04-03 04:25:10 +0500

    View All degrees done

--- src/app/university/university-view-degrees/university-view-degrees.component.ts
+++ src/app/university/university-view-degrees/university-view-degrees.component.ts
@@ -12,7 +12,9 @@ import { CollapseDirective } from 'angular-bootstrap-md';
 export class UniversityViewDegreesComponent implements OnInit {
 
   filterVar: string;
-  degrees: Degree[];
+  degrees: any;
+  formFieldUniKeys: any;
+  formFieldUniValues: any;
   formFields: any;
   degreeDetail: Degree;
   rowCollapsed: boolean;
@@ -31,12 +33,60 @@ export class UniversityViewDegreesComponent implements OnInit {
   //   this.router.navigateByUrl('university/detail/' + id);
 
   // }
+  makeInitCapWithSpace(x): any{
+    var initCap="";
+   var lowerCase=0;
+   for(var i=0; i<x.length; i++){
+       if(x.charCodeAt(i)>90)
+           lowerCase=1;
+   }
+   if(lowerCase===0)
+       return x;
+
+   initCap=initCap.concat(String.fromCharCode(x.charCodeAt(0)-32));
+   var j=1;
+   for(var i=1; i<x.length; i++){
+       if(x.charCodeAt(i)<97) {
+           initCap=initCap.concat(" ");
+       }
+       initCap=initCap.concat(x.charAt(i));
+   }
+   return initCap;
+
+  }
+  createFormFieldUni(): void{
+    this.formFieldUniKeys = []; 
+    this.formFieldUniValues = [];
+    this.degrees.forEach((degree, index)=>{
+      this.formFieldUniKeys[index]=Object.keys(degree.degreeDetails);
+      if(this.formFieldUniKeys[index]["universityId"]){
+        delete this.formFieldUniKeys[index]["universityId"];
+      }
+      this.formFieldUniValues[index]=[];
+      this.formFieldUniKeys[index].forEach((key,index2) => {
+        this.formFieldUniValues[index][index2] = degree.degreeDetails[key];
+      });
+      this.formFieldUniKeys[index]=this.formFieldUniKeys[index].map((key)=>{
+        
+        return this.makeInitCapWithSpace(key);
+      })
+      console.log("blaolaoalaoal");
+      console.log(this.formFieldUniKeys[index]);
+        })
+  }
 
   getDegrees(): void {
     this.UniversitydegreeService.getDegrees()
     .subscribe(degrees => {
       console.log(degrees);
-      this.degrees = degrees},
+      this.degrees = degrees;
+      this.createFormFieldUni();
+      setTimeout(()=>{
+        console.log("FormformFieldUniKeys",this.formFieldUniKeys );
+        console.log("formFieldUniValues", this.formFieldUniValues );
+
+      },3000)
+    },
       error => {
         console.error(error);
       }
@@ -46,6 +96,10 @@ export class UniversityViewDegreesComponent implements OnInit {
     this.UniversitydegreeService.getFormFields.subscribe(
       response => {
         this.formFields=response.body;
+        var temp={
+          name: "Status"
+        };
+        this.formFields[this.formFields.length] =temp;
         console.log(response);
       },
       error => {
